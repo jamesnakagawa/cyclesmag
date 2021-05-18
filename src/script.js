0d10f3067c48f10766a4a21fb4847b9cc8b0cb38
James Nakagawa <me@james.computer>
2021-05-17 21:49:42 -0700

    Flicker effect, add poem, and CSS cleanup

--- src/script.js
+++ src/script.js
@@ -10,16 +10,34 @@ import {makeNoise} from './noise';
 document.body.classList.remove('preload');
 document.querySelector('.title').classList.add('active');
 
-window.scroll = new LocomotiveScroll({
+const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
     lerp: 0.7,
     direction: 'horizontal'
 });
-window.scroll.init();
+
+scroll.init();
 
 drawBall('canvas.ball');
 drawCube('canvas.box');
 // makeNoise('noise');
+
+const timings = [500,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,100,100,500,40,40];
+scroll.on('call', function(event_name, direction, obj) {
+    if (event_name === 'flicker') {
+        const timings_ = [...timings];
+        (function light_switch() {
+            const nextInterval = timings_.shift();
+            const tail = () => {
+                obj.el.classList.toggle('bright');
+                light_switch();
+            }
+            if (nextInterval)
+                setTimeout(tail, nextInterval);
+        })();
+        obj.el
+    }
+})
 
 //initText();
